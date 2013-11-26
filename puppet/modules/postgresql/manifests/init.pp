c8f68a986b2c8a2243e6b81af61a982cb281be84
galador <git@galador.org>
2013-11-25 20:59:03 -0600

    Add postgres configuration

---
+++ puppet/modules/postgresql/manifests/init.pp
@@ -0,0 +1,17 @@
+# Install PostgreSQL 9.3 server from the PGDG repository
+class {'postgresql::globals':
+  version => '9.3',
+  manage_package_repo => true,
+  encoding => 'UTF8',
+  locale  => 'it_IT.utf8',
+}->
+class { 'postgresql::server':
+  ensure => 'present',
+  listen_addresses => '*',
+  manage_firewall => true,
+}
+
+# Install contrib modules
+class { 'postgresql::server::contrib':
+  package_ensure => 'present',
+}
