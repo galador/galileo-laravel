42494f94afd96f18730e7b31c2e06ca0d19b6e85
galador <git@galador.org>
2013-11-25 22:20:32 -0600

    Wrap the puppetlabs/postgresql commands for installation in the `postgresql` class

--- puppet/modules/postgresql/manifests/init.pp
+++ puppet/modules/postgresql/manifests/init.pp
@@ -1,17 +1,19 @@
-# Install PostgreSQL 9.3 server from the PGDG repository
-class {'postgresql::globals':
-  version => '9.3',
-  manage_package_repo => true,
-  encoding => 'UTF8',
-  locale  => 'en_US.utf8',
-}->
-class { 'postgresql::server':
-  ensure => 'present',
-  listen_addresses => '*',
-  manage_firewall => true,
-}
+class postgresql {
+	# Install PostgreSQL 9.3 server from the PGDG repository
+	class {'postgresql::globals':
+	  version => '9.3',
+	  manage_package_repo => true,
+	  encoding => 'UTF8',
+	  locale  => 'en_US.utf8',
+	}->
+	class { 'postgresql::server':
+	  ensure => 'present',
+	  listen_addresses => '*',
+	  manage_firewall => true,
+	}
 
-# Install contrib modules
-class { 'postgresql::server::contrib':
-  package_ensure => 'present',
+	# Install contrib modules
+	class { 'postgresql::server::contrib':
+	  package_ensure => 'present',
+	}
 }
